1e3b59eb140658386c3caac39731ee60d251251b
Felix Jacobsen <felix.jacobsen@iths.se>
2022-04-23 14:14:51 +0200

    Add dependency and create Scanner and ServiceLoader fields in Main

--- Consumer/src/main/java/iths/webservice/consumer/Main.java
+++ Consumer/src/main/java/iths/webservice/consumer/Main.java
@@ -1,7 +1,15 @@
 package iths.webservice.consumer;
 
+import iths.webservice.convert.Converter;
+
+import java.util.Scanner;
+import java.util.ServiceLoader;
+
 public class Main {
+    private final Scanner scanner = new Scanner(System.in);
+    private final ServiceLoader<Converter> serviceLoader = ServiceLoader.load(Converter.class);
+
     public static void main(String[] args) {
-        
+
     }
 }
