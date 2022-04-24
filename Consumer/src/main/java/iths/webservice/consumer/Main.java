6b9917349c593fc69c0dec2b2d230ca312eca52f
Felix Jacobsen <felix.jacobsen@iths.se>
2022-04-24 21:11:25 +0200

    Implement to print Designation

--- Consumer/src/main/java/iths/webservice/consumer/Main.java
+++ Consumer/src/main/java/iths/webservice/consumer/Main.java
@@ -1,6 +1,7 @@
 package iths.webservice.consumer;
 
 import iths.webservice.convert.Converter;
+import iths.webservice.convert.Designation;
 
 import java.util.List;
 import java.util.Scanner;
@@ -13,23 +14,28 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         main.run();
-
     }
 
     private void run(){
         List<Converter> converterList = getConverterList();
         int option;
+
         while(true) {
             double celsius = getCelsius();
             try {
                 option = getChoice(converterList);
                 if (option == 0)
                     break;
+
                 Converter converter = converterList.get(option - 1);
                 printAfterConvert(converter, celsius);
+                printDesignation(converter);
 
             } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("Enter [1] to convert into fahrenheit or [2] to convert into kelvin");
+                System.out.println("Enter 1 to convert into fahrenheit or 2 to convert into kelvin");
+
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
             }
             System.out.println("Type 0 to exit the program" + "\n" + "or");
         }
@@ -47,18 +53,23 @@ public class Main {
     }
 
     private int getChoice(List<Converter> converterList) {
-        printChoices(converterList);
+        printInformation(converterList);
         return Integer.parseInt(scanner.nextLine());
     }
 
 
-    private void printChoices(List<Converter> converterList) {
+    private void printInformation(List<Converter> converterList) {
         for (Converter c : converterList)
             System.out.println(c.information());
     }
 
     private void printAfterConvert(Converter converter, double celsius) {
-        System.out.println("Result: " + converter.convert(celsius));
+        System.out.print("Result: " + converter.convert(celsius));
     }
 
+    private void printDesignation(Converter converter) throws NoSuchMethodException {
+        var annotation = converter.getClass().getMethod("convert", double.class).getAnnotation(Designation.class);
+        if (annotation != null)
+            System.out.println(annotation.value());
+    }
 }
